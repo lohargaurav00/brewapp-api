3e4f0d2d3832bb3ba31e7786099903090f613c33
Gaurav Lohar <lohargaurav00@gmail.com>
2023-11-08 14:21:03 +0530

    + model -> book

---
+++ src/models/book.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const AddBookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String,
+        required: true,
+    },
+    summary: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+        required: true,
+    },
+});
+
+const Book = mongoose.model("book", AddBookSchema);
+module.exports = Book;
