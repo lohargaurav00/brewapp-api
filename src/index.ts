cd75893a1dee455af5d5421f9b7ef9cb92d922b2
Gaurav Lohar <lohargaurav00@gmail.com>
2023-11-08 16:00:05 +0530

    + @Route -> find-book

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import connectToDb from "./db";
 //@Routes import
 import addBook from "./routes/add-book";
 import viewAllBooks from "./routes/view-all-books";
+import findBook from "./routes/find-book";
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -22,6 +23,7 @@ app.get("/", (req: Request, res: Response) => {
 // @Routes
 app.use("/api/add-book", addBook); // adds a book
 app.use("/api/view-all-books", viewAllBooks); // returns list of all books
+app.use("/api/find-book", findBook); // returns a list of books with a query id or title or author
 
 app.listen(port, () => {
   console.log("Server is running on port: " + port);
---
+++ src/routes/find-book.ts
@@ -0,0 +1,58 @@
+import { Router, Request, Response } from "express";
+import { query, oneOf, validationResult } from "express-validator";
+
+import Book from "../models/book";
+
+const router = Router();
+
+// @route   GET api/find-book
+// @desc    find a book with a query id or title or author and return it
+// @access  Public
+
+router.get(
+  "/",
+  oneOf(
+    [
+      query("id", "Please enter a valid id").isMongoId(),
+      query("title", "Please enter a valid title").isString(),
+      query("author", "Please enter a valid author").isString(),
+    ],
+    {
+      message: "Please enter a valid id or title or author",
+      errorType: "least_errored",
+    }
+  ),
+  async (req: Request, res: Response) => {
+    try {
+      // If there are errors return Bad request and the errors
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const { id, title, author } = req.query;
+
+      // search for a book with the id or title or author
+      const books = await Book.find(
+        id ? { _id: id } : title ? { title } : { author }
+      );
+
+      // if there are no books return Bad request
+      if (books.length === 0) {
+        return res.status(400).json({
+          response: "error",
+          message: `There are no books in the associated with this ${
+            id ? id : title ? title : author
+          }`,
+        });
+      }
+      // return the list of founded books
+      res.status(200).json(books);
+    } catch (error: any) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+
+export default router;
