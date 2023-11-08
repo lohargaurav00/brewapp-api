f9206975e2bf43dd3f1d481311d65bc9979089fb
Gaurav Lohar <lohargaurav00@gmail.com>
2023-11-08 18:20:56 +0530

    + reconnect and exit

--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,28 @@
 import mongoose from "mongoose";
 
-const DbUrl = process.env.MONGO_DB_URL || ''
+const DbUrl = process.env.MONGO_DB_URL || "";
+
+let connectionAttempts = 0;
 
 export default function connectToDb() {
-    mongoose.connect(DbUrl).then(() => {
-        console.log("Connected to database");
-    }).catch((err) => {
-        console.log("Error connecting to database", err.message);
+  mongoose
+    .connect(DbUrl)
+    .then(() => {
+      console.log("Connected to database");
     })
+    .catch((err) => {
+      console.log("Error connecting to database", err.message);
+
+      // If connection fails, try again after 5 seconds and limit the number of connection attempts to 3
+      if (connectionAttempts < 3) {
+        setTimeout(() => {
+          console.log("Attempting to reconnect...");
+          connectToDb();
+          connectionAttempts++;
+        }, 5000);
+      } else {
+        console.log("Max connection attempts reached. Exiting...");
+        process.exit(1);
+      }
+    });
 }
