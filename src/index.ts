0355f6dd9eea2a9d5bd2a11a6a89f32ebb1756bd
Gaurav Lohar <lohargaurav00@gmail.com>
2023-11-08 15:07:27 +0530

    + @Route -> view-all-books

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express, { Response, Request } from 'express';
-import cors from 'cors';
-import 'dotenv/config';
+import express, { Response, Request } from "express";
+import cors from "cors";
+import "dotenv/config";
 
-import connectToDb from './db';
+import connectToDb from "./db";
 
 //@Routes import
-import addBook from './routes/add-book';
-
+import addBook from "./routes/add-book";
+import viewAllBooks from "./routes/view-all-books";
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -15,13 +15,14 @@ connectToDb();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Welcome to the Book API');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to the Book API");
 });
 
-app.use('/api/add-book', addBook);
+// @Routes
+app.use("/api/add-book", addBook); // adds a book
+app.use("/api/view-all-books", viewAllBooks); // returns list of all books
 
 app.listen(port, () => {
-    console.log('Server is running on port: ' + port);
+  console.log("Server is running on port: " + port);
 });
-
---
+++ src/routes/view-all-books.ts
@@ -0,0 +1,27 @@
+import { Router, Request, Response } from "express";
+
+import Book from "../models/book";
+
+const router = Router();
+
+// @route   POST api/view-all-books
+// @desc    List all books
+// @access  Public
+
+router.get("/", async (req: Request, res: Response) => {
+  try {
+    const books = await Book.find();
+    if(books.length === 0) {
+        return res.status(400).json({
+            response: "error",
+            message: "There are no books in the database",
+        });
+    }
+    res.status(200).json(books);
+  } catch (error: any) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+export default router;
